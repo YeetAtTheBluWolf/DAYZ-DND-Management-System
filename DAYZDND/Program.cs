721cddb909d0cf4f437a4ce9878b3454021fc58f
Richard Bates <yeetatthebluewolf@gmail.com>
2023-02-08 21:03:00 -0500

    Add project files.

---
+++ DAYZDND/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DAYZDND.util;
+
+namespace DAYZDND;
+
+public class Program
+{
+    
+    public static void Main()
+    {
+        var numOfUsers = UserInput.Int("Enter a number of users: ");
+        string[] usernames = new string[numOfUsers];
+        for (int i = 0; i < numOfUsers; i++)
+        {
+            usernames[i] = UserInput.Line("Enter a name for user #" + (i + 1) + ": ");
+        }
+    }
+
+}
---
+++ DAYZDND/util/UserInput.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable CS8604 
+#pragma warning disable CS8603
+
+namespace DAYZDND.util
+{
+    public class UserInput
+    {
+        public static int Int(string question)
+        {
+            try
+            {
+                Console.Write(question);
+                return int.Parse(Console.ReadLine());
+            } catch
+            {
+                return Int(question);
+            }
+        }
+
+        public static string Line(string question)
+        {
+            Console.Write(question);
+            return Console.ReadLine();
+        }
+
+    }
+}
